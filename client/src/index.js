a281306e545ff2045258a5ffaf902b0b716c6b96
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-08 15:15:28 +0700

    fix import

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStoreHook, applyMiddleware, compose} from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from "redux-thunk";
 
 import reducers from "./reducers";
 
 import App from './App';
 
-const store = createStoreHook(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
