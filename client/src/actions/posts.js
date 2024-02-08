da6cbe95fe1ef503352182052b63a870142df3a9
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-08 15:08:34 +0700

    fetch data and dispatch

---
+++ client/src/actions/posts.js
@@ -0,0 +1,11 @@
+import * as api from "../api";
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({type: "FETCH_ALL", payload: []});
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+}
