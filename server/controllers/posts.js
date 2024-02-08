57c433324685b50d0e44cd0d2cd83fa6ccc92112
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-08 11:17:22 +0700

    log postMessage

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,18 @@
- 
+import PostMessage from "../models/postMessage.js"
 
-export const getPosts = (req,res)=>{
-    res.send("HOMEPAGE")
+export const getPosts = async (req,res)=>{
+    try {
+        const postMessage = await PostMessage.find();
+
+        console.log(postMessage);
+
+        res.status(200).json(postMessage);
+
+    } catch (error) {
+        res.status(404).json({message: error.message});        
+    }
 }
 
 export const createPosts = (req,res) => {
-    res.send("Post creation")
+    
 }
