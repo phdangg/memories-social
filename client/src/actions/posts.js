47a98a1ecbe537aefe8e6eab15e2d326b3a447e8
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-11 15:45:41 +0700

    pass post as arg

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -9,7 +9,7 @@ export const getPosts = () => async (dispatch) => {
     }    
 }
 
-export const createPost = () => async (dispatch) => {
+export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
 
