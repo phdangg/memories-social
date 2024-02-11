1e71477647590be0141949036717c457e9850879
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-11 15:05:58 +0700

    dispatch data

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -3,9 +3,8 @@ import * as api from "../api";
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        dispatch({type: "FETCH_ALL", payload: []});
+        dispatch({type: "FETCH_ALL", payload: data});
     } catch (error) {
-        console.log(error.message);
-    }
-    
+        console.log(error.message); 
+    }    
 }
