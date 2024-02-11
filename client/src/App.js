988ee8e5744605ed610d63eb176ca19706252004
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-11 17:01:16 +0700

    style change

--- client/src/App.js
+++ client/src/App.js
@@ -24,11 +24,11 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts/>        
                         </Grid>
-                        <Grid item xs={12} sm={7}>
+                        <Grid item xs={12} sm={4}>
                             <Form/>                            
                         </Grid>
                     </Grid>
