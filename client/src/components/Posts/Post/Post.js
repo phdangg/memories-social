c555694e51fa213aca2996b19f5b50cc4e5cc301
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-18 14:39:39 +0700

    call delete post action

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,9 +5,12 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizICon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import { deletePost } from "../../../actions/posts";
 
 const Post = ({post, setCurrentId}) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>            
@@ -32,7 +35,7 @@ const Post = ({post, setCurrentId}) => {
                     <ThumbUpAltIcon fontSize="small"/>
                 Like {post.likedCount}
                 </Button>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=>dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small"/>
                     Delete
                 </Button>
