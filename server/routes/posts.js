54d82b0fb0e428c6cda15e3e73021e304821a70c
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-18 14:57:31 +0700

    change like post route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPosts);
 router.patch('/:id', updatePost)
-router.patch('/:id/likedPost', likePost)
+router.patch('/:id/likePost', likePost)
 router.delete('/:id', deletePost)
 
 export default router;
