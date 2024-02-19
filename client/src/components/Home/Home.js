130b8f7f0c8d1daac916268b0d75faea2381189a
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-19 23:36:48 +0700

    fetch all posts logic

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Grow, Grid } from "@material-ui/core";
+import { getPosts } from "./actions/posts";
+import { useDispatch } from "react-redux";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getPosts());
+    },[currentId,dispatch]);
+    
   return (
     <Grow in>
         <Container>
