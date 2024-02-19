a38588b163bef9f95b3ae9fb883999247ae7f6a0
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-19 23:40:23 +0700

    route change:

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { getPosts } from "./actions/posts";
-
-import useStyles from "./styles";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 
 const App = () => {    
 
     return (
-        <Container maxWidth="lg">           
-            <NavBar />
-            <Home/>
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth="lg">           
+                <NavBar />
+                <Switch>
+                    <Route path="/" exact Component={Home}/>
+                    <Route path="/auth" exact Component={Auth}/>
+                </Switch>
+                <Home/>
+            </Container>
+        </BrowserRouter>
     )
 }
 
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import memories from "../../images/memories.png"
 
 const NavBar = () => {
     const classes = useStyles();
 
-    return (
+    const user = null;
+
+    return (        
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>        
-            <img className={classes.image} src={memories} alt="memories" height="60"/>
+            <div className={classes.brandContainer}>
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>        
+                <img className={classes.image} src={memories} alt="memories" height="60"/>
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+                        <Button variant='contained' className={classes.logout} color='secondary'>Logout</Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color='primary'>Sign in</Button>
+                )} 
+            </Toolbar>
         </AppBar>
     );
 }
