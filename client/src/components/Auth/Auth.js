9e10010fa8943bfb4cd0b41f3bb4d9474e85984c
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-21 08:54:56 +0700

    google sign in button

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from "@material-ui/core";
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField, Icon } from "@material-ui/core";
+import { GoogleLogin } from "react-google-login";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import useStyles from "./styles";
 import Input from "./Input";
@@ -47,6 +48,20 @@ const Auth = () => {
             <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword}/>
             {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password"/>}
           </Grid>
+          <GoogleLogin
+            clientId='GOOGLE ID'
+            render={(renderProps)=>(
+              <Button 
+                  className={classes.googleButton} 
+                  variant='contained' color='primary' 
+                  onClick={renderProps.onClick} 
+                  disabled={renderProps.disabled} 
+                  startIcon={<Icon/>}
+                >
+                  Google Sign In
+              </Button>
+            )}
+          />
           <Button type="submit" fullWidth variant='contained' color="primary" className={classes.submit}>
             {isSignup ? 'Sign Up' : 'Sign In'}
           </Button>
