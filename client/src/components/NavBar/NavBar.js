bcb804192481b313cea94afe1a520f37fb3738b2
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-25 19:54:55 +0700

    correct data access

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
@@ -6,8 +6,8 @@ import memories from "../../images/memories.png"
 
 const NavBar = () => {
     const classes = useStyles();
-
-    const user = null;
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    
 
     return (        
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -18,8 +18,8 @@ const NavBar = () => {
             <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                        <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+                        <Avatar className={classes.purple} alt={user.name} src={user.picture}>{user.family_name}</Avatar>
+                        <Typography className={classes.userName} variant='h6'>{user.name}</Typography>
                         <Button variant='contained' className={classes.logout} color='secondary'>Logout</Button>
                     </div>
                 ) : (
