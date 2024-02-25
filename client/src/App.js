ea0890c5e136488bfc57550375a18cbce3e3607e
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-25 19:53:28 +0700

    fetch google client id and use OAuth provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "@material-ui/core";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import axios from "axios";
+
 
 const App = () => {    
+    const [clientId, setClientId] = useState(null);
+    
+    useEffect(()=>{
+        axios.get("http://localhost:5000/client_id")
+            .then((res)=>setClientId(res.data.client_id));
+    },[clientId])      
 
     return (
+        <GoogleOAuthProvider clientId={clientId}>
         <BrowserRouter>
             <Container maxWidth="lg">           
                 <NavBar />
@@ -18,6 +27,7 @@ const App = () => {
                 </Routes>                
             </Container>
         </BrowserRouter>
+        </GoogleOAuthProvider>
     )
 }
 
