3813d135178b47e9d9743730b30eb3fbd8d541ca
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-02-25 19:55:44 +0700

    auth and logout type

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,7 +1,7 @@
-const CREATE = "CREATE";
-const UPDATE = "UPDATE";
-const DELETE = "DELETE";
-const FETCH_ALL = "FETCH_ALL";
-const LIKE = "LIKE";
-
-export {CREATE,UPDATE,DELETE,FETCH_ALL,LIKE} 
+export const CREATE = "CREATE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DELETE";
+export const FETCH_ALL = "FETCH_ALL";
+export const LIKE = "LIKE";
+export const AUTH = "AUTH";
+export const LOGOUT = "LOGOUT";
