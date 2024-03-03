2c1b39b834f1c65d29b22ddd4642d244aa694a63
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-03 17:20:01 +0700

    auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500;
+        // There 2 auth, OAuth from Google and our custom Auth
+
+        let decodedData;
+
+        if (token && isCustomAuth) {
+            decodedData = jwt.verify(token, 'test');
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(token);
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default auth;
