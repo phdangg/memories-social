1a1326c5d884af940de2830d6fad030e6bcc4874
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-03 07:49:22 +0700

    implement signIn func

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,7 +4,26 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
 export const signIn = async (req, res) => {
-    
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser)
+            return res.status(404).json({ message: "User doesn't exist."});
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+        if (!isPasswordCorrect)
+            return res.status(404).json({ message: "Invalid credentials."});
+
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id}, 'test', {expiresIn: "1h"});
+
+        res.status(200).json({ result: existingUser, token});
+                
+    } catch (error) {
+        res.status(500).json({ message: "Something wrong!"});
+    }
 }
 
 export const signUp = async (req, res) => {
