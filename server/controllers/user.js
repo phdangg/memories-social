a7a8598a45f4f732c66b3f99158d7f5e039436b4
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-03 08:04:45 +0700

    implement signUp func

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -27,5 +27,27 @@ export const signIn = async (req, res) => {
 }
 
 export const signUp = async (req, res) => {
+    const { email, password, confirmPassword,firstName, lastName } = req.body;
     
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser)
+            return res.status(404).json({ message: "User already exists."});
+
+        if (password !== confirmPassword)
+            return res.status(404).json({ message: "Passwords don't match."});
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`})
+
+        const token = jwt.sign({ email: result.email, id: result._id}, 'test', {expiresIn: "1h"});
+
+        res.status(200).json({ result, token});
+
+
+    } catch (error) {
+        res.status(500).json({ message: "Something wrong!"});        
+    }
 }
