0f9a6041db00ce0faa09fb5a301182f6c4a0dfd3
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-03 17:36:13 +0700

    create sign in sign up api func

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,8 +3,11 @@ import axios from "axios";
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 
-export const fetchPosts = () => API.get(url);
-export const createPost = (newPost) => API.post(url,newPost);
-export const updatePost = (id, updatePost) => API.patch(`${url}/${id}`, updatePost);
-export const deletePost = (id) => API.delete(`${url}/${id}`);
-export const likePost = (id) => API.patch(`${url}/${id}/likePost`);
+export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts',newPost);
+export const updatePost = (id, updatePost) => API.patch(`/posts/${id}`, updatePost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+
+export const signIn = (formData) => API.post('/users/signin',formData);
+export const signUp = (formData) => API.post('/users/signup',formData);
