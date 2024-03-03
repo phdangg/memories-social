339109b8359d6ff25df50fcddc16f5890fafe2de
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-03 07:41:56 +0700

    empty signIn and signUp func

---
+++ server/controllers/user.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.js";
+
+export const signIn = async (req, res) => {
+    
+}
+
+export const signUp = async (req, res) => {
+    
+}
