275b39022939025370513d7f2d0b5725eb8d4202
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-10 19:30:36 +0700

    format change: indent

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -53,7 +53,7 @@ const Post = ({post, setCurrentId}) => {
                 </Button>
                 {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
                     <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize="small" /> Delete
+                        <DeleteIcon fontSize="small" /> Delete
                     </Button>
                 )}
             </CardActions>
