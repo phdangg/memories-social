40046ae57fb4f1176fc04e7abc8e3dc041023e5a
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-16 22:11:45 +0700

    correct navigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, redirect, Navigate  } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 import axios from "axios";
 
 
 const App = () => {    
     const [clientId, setClientId] = useState(null);
+    const user = JSON.parse(localStorage.getItem('profile'));
     
     useEffect(()=>{
         axios.get("http://localhost:5000/client_id")
             .then((res)=>setClientId(res.data.client_id));
-    },[clientId])      
+    },[clientId])     
+
 
     return (
         <GoogleOAuthProvider clientId={clientId}>
@@ -22,8 +25,11 @@ const App = () => {
             <Container maxWidth="lg">           
                 <NavBar />
                 <Routes>
-                    <Route path="/" exact Component={Home}/>
-                    <Route path="/auth" exact Component={Auth}/>
+                    <Route path="/" exact element={<Navigate to="/posts" />} />
+                    <Route path="/posts" exact Component={Home}/>
+                    <Route path="/posts/search" exact Component={Home}/>
+                    <Route path="/posts/:id" Component={PostDetails}/>
+                    <Route path="/auth" exact Component={()=>(!user ? <Auth/> : <Navigate to="/posts" />)}/>
                 </Routes>                
             </Container>
         </BrowserRouter>
