4311a0bf413ffe8396183c78f97402ae637a1679
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-17 20:19:12 +0700

    indent change and make post be able on click

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -34,26 +34,27 @@ const Post = ({post, setCurrentId}) => {
 
     return (
         <Card className={classes.card} raised elevation={6}>
-        <ButtonBase className={classes.cardActions} onClick={openPost} />
-            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>            
-            <div className={classes.overlay}>
-                <Typography variant="h6">{post.name}</Typography>
-                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-            </div>
-            {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
-                <div className={classes.overlay2}>
-                    <Button style={{color: "white"}} size="small" onClick={()=>{setCurrentId(post._id)}}>
-                        <MoreHorizICon fontSize="default"/>
-                    </Button>
+            <ButtonBase className={classes.cardActions} onClick={openPost}>
+                <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>            
+                <div className={classes.overlay}>
+                    <Typography variant="h6">{post.name}</Typography>
+                    <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
                 </div>
-            )}            
-            <div className={classes.details}>
-                <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=>`#${tag} `)}</Typography>
-            </div>
-            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
-            </CardContent>
+                {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+                    <div className={classes.overlay2}>
+                        <Button style={{color: "white"}} size="small" onClick={()=>{setCurrentId(post._id)}}>
+                            <MoreHorizICon fontSize="default"/>
+                        </Button>
+                    </div>
+                )}            
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=>`#${tag} `)}</Typography>
+                </div>
+                <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+                </CardContent>
+            </ButtonBase>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" disabled={!user?.result} onClick={()=>dispatch(likePost(post._id))}>
                     <Likes/>
