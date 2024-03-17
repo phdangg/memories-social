d168628c1a32fb9b6c61156f48e2d9968ceda4ba
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-17 21:25:16 +0700

    handle fetch post detail and comments

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,21 @@
 import express from "express";
-import { getPosts, createPosts, updatePost, deletePost, likePost, getPostsBySearch } from "../controllers/posts.js";
+import { getPost, getPostsByCreator, commentPost, getPosts, createPosts, updatePost, deletePost, likePost, getPostsBySearch } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router(); 
 
 router.get('/', getPosts);
+router.get('/:id', getPost);
+router.get('/creator', getPostsByCreator);
+router.get('/search', getPostsBySearch);
+
 router.post('/', auth, createPosts);
-router.patch('/:id', auth, updatePost)
-router.patch('/:id/likePost', auth, likePost)
-router.delete('/:id', auth, deletePost)
-router.get('/search',getPostsBySearch);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', commentPost);
+
+
+
+
 export default router;
