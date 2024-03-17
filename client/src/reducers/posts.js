e43b16fa54b9a20d6dfedb6cdba3e40ad6ca9f89
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-17 19:05:38 +0700

    FETCH_BY_SEARCH action

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,9 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes";
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_BY_SEARCH } from "../constants/actionTypes";
 const postsReducer = (posts = [], action) => {
     switch (action.type) {
         case FETCH_ALL:
+            return action.payload;
+        case FETCH_BY_SEARCH:
             return action.payload;
         case CREATE:
             return [...posts, action.payload];  
