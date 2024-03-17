c552427923739ccaf5c087f86af77b065dc143bb
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-03-17 19:35:39 +0700

    delete title

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -35,7 +35,6 @@ const NavBar = () => {
     return (        
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>        
                 <img className={classes.image} src={memories} alt="memories" height="60"/>
             </div>
             <Toolbar className={classes.toolbar}>
